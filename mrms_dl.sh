7122c5910c5a43c429fd1bee0ee4f13e1c82a01b
JFMass <jfmassicotte@quebecvortex.com>
2018-06-28 05:57:16 -0400

    Update mrms_dl.sh

--- mrms_dl.sh
+++ mrms_dl.sh
@@ -1,6 +1,16 @@
-
+#!/usr/bin/env bash
 
 #this file will downlaod mrms files
+
+
+
+
+tstamp=$(date +%Y%m%d%H%M)
+
+wget http://mrms.ncep.noaa.gov/data/2D/SeamlessHSR/MRMS_SeamlessHSR.latest.grib2.gz
+
+gunzip MRMS_SeamlessHSR.latest.grib2.gz
+
 
 
 
