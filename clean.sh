3be295ef7f27c5df65ef839aec19e3cdfe2294ce
JFMass <jfmassicotte@quebecvortex.com>
2018-06-29 02:09:14 -0400

    Update clean.sh

--- clean.sh
+++ clean.sh
@@ -8,7 +8,7 @@ chkdate=`date --date="2 days ago" +%y%m%d`
 
 if [ ! -z $(grep "$chkdate" archive.txt) ]
 then
-    echo "Date is archived, will not delete but move to archives"
+    echo "Archiving $chkdate"
     mkdir /home/meteo/html/models/archives/"$chkdate"
     x=0
     while [ $x -lt 24 ]
@@ -19,6 +19,7 @@ then
     done
 else
     x=0
+    echo "Deleting $chkdate"
     while [ $x -lt 24 ]
     do
     del=$(date -d "$chkdate 12:00 UTC +$x hour" +%Y%m%d%H)
@@ -27,8 +28,6 @@ else
     done
 fi
 
-echo $chkdate
-echo "Done deleting date"
 
 
 
